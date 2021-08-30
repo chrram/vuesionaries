717e0eefcb5ff8ad314ab3e4675d34d2eb2f4de0
chrram <chr.ram.94@gmail.com>
2021-08-30 20:00:00 +0200

    The project is initialized.

---
+++ App.vue
@@ -0,0 +1,19 @@
+<template>
+  <view class="container">
+    <text class="text-color-primary">Here goes the new project</text>
+    <text class="text-color-primary">It is gonna be a vuetiful app!</text>
+    <text class="text-color-primary">Greetings from Chris!</text>
+  </view>
+</template>
+
+<style>
+.container {
+  background-color: white;
+  align-items: center;
+  justify-content: center;
+  flex: 1;
+}
+.text-color-primary {
+  color: red;
+}
+</style>
